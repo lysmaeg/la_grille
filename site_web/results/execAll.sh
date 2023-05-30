a955cb7318106ec5873ddbf1fa7be50def3e816d
Da Silva David <ddasilva@starwars.ETUD>
2023-05-30 12:01:02 +0200

    Added mysetes

--- site_web/results/execAll.sh
+++ site_web/results/execAll.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
 
-../../build/theGrid ../instances/competition/competition_03.txt -o 03_1 -t 240
-../../build/theGrid ../instances/competition/competition_04.txt -o 04_1 -t 240
-../../build/theGrid ../instances/competition/competition_05.txt -o 05_1 -t 240
-../../build/theGrid ../instances/competition/competition_06.txt -o 06_1 -t 240
-../../build/theGrid ../instances/competition/competition_07.txt -o 07_1 -t 240
-../../build/theGrid ../instances/competition/competition_08.txt -o 08_1 -t 240
-../../build/theGrid ../instances/competition/competition_09.txt -o 09_1 -t 240
-../../build/theGrid ../instances/competition/competition_10.txt -o 10_1 -t 240
-../../build/theGrid ../instances/competition/competition_11.txt -o 11_1 -t 240
-../../build/theGrid ../instances/competition/competition_12.txt -o 12_1 -t 240
-../../build/theGrid ../instances/competition/competition_13.txt -o 13_1 -t 240
-../../build/theGrid ../instances/competition/competition_14.txt -o 14_1 -t 240
-../../build/theGrid ../instances/competition/competition_15.txt -o 15_1 -t 240
+#../../build/theGrid ../instances/competition/competition_03.txt -o 03_1 -t 240
+#../../build/theGrid ../instances/competition/competition_04.txt -o 04_1 -t 240
+#../../build/theGrid ../instances/competition/competition_05.txt -o 05_1 -t 240
+#../../build/theGrid ../instances/competition/competition_06.txt -o 06_1 -t 240
+#../../build/theGrid ../instances/competition/competition_07.txt -o 07_1 -t 240
+#../../build/theGrid ../instances/competition/competition_08.txt -o 08_1 -t 240
+#../../build/theGrid ../instances/competition/competition_09.txt -o 09_1 -t 240
+#../../build/theGrid ../instances/competition/competition_10.txt -o 10_1 -t 240
+#../../build/theGrid ../instances/competition/competition_11.txt -o 11_1 -t 240
+#../../build/theGrid ../instances/competition/competition_12.txt -o 12_1 -t 240
+#../../build/theGrid ../instances/competition/competition_13.txt -o 13_1 -t 240
+#../../build/theGrid ../instances/competition/competition_14.txt -o 14_1 -t 240
+#../../build/theGrid ../instances/competition/competition_15.txt -o 15_1 -t 240
+
+../../build/theGrid ../instances/mystere/mystere_01.txt -o m01 -t 30
+../../build/theGrid ../instances/mystere/mystere_02.txt -o m02 -t 30
+../../build/theGrid ../instances/mystere/mystere_03.txt -o m03 -t 30
+../../build/theGrid ../instances/mystere/mystere_04.txt -o m04 -t 30
+../../build/theGrid ../instances/mystere/mystere_05.txt -o m05 -t 30
+../../build/theGrid ../instances/mystere/mystere_06.txt -o m06 -t 30
+../../build/theGrid ../instances/mystere/mystere_07.txt -o m07 -t 30
+../../build/theGrid ../instances/mystere/mystere_08.txt -o m08 -t 30
+../../build/theGrid ../instances/mystere/mystere_09.txt -o m09 -t 30
+../../build/theGrid ../instances/mystere/mystere_10.txt -o m10 -t 30
